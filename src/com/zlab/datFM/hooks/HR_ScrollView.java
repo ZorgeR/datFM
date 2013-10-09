4cfa7afd57d35f71f09b297f06566a4c0eea2ec0
Zorge.R <me@z-lab,me>
2013-10-09 21:25:40 +0400

    0.1.85b - try to fix horizontal swipe

--- src/com/zlab/datFM/hooks/HR_ScrollView.java
+++ src/com/zlab/datFM/hooks/HR_ScrollView.java
@@ -2,13 +2,19 @@ package com.zlab.datFM.hooks;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.HorizontalScrollView;
 
 public class HR_ScrollView extends HorizontalScrollView {
 
     private HR_ScrollViewListener scrollViewListener = null;
     private boolean scrolled = true;
+    private GestureDetector mGestureDetector;
+    View.OnTouchListener mGestureListener;
+
 
     public void setIsScrolled(boolean enabled) {
         scrolled = enabled;
@@ -27,6 +33,7 @@ public class HR_ScrollView extends HorizontalScrollView {
 
     public HR_ScrollView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mGestureDetector = new GestureDetector(context, new YScrollDetector());
     }
 
     public void setScrollViewListener(HR_ScrollViewListener scrollViewListener) {
@@ -55,7 +62,20 @@ public class HR_ScrollView extends HorizontalScrollView {
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {      /*
         if (!scrolled) return false;
-        else */return super.onInterceptTouchEvent(ev);
+        else */return super.onInterceptTouchEvent(ev) && mGestureDetector.onTouchEvent(ev);
+    }
+
+    class YScrollDetector extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            //Log.e("Distance X=",String.valueOf(distanceX));
+            //Log.e("Distance Y=",String.valueOf(distanceY));
+            if(Math.abs(distanceX) > Math.abs(distanceY)) {
+                return true;
+            }
+
+            return false;
+        }
     }
 
 
